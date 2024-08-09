f72ac154fc1a30c397690bc1638024490baecc3f
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-08-09 12:53:47 -0400

    Módulo para cambiar src. Clases (faltan métodos).

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,39 @@
 /*
+https://www.youtube.com/embed/6BUgYDmlVow
 */
 
-class Multimedia{}
+const moduloSrc = (()=>{
+    function setSrcPrivate(url, id){
+        document.getElementById(id).setAttribute("src",url);
+    }
+    return {
+        setSrc: (url, id)=>{
+            setSrcPrivate(url, id);
+        }
+    }
+})();
+
+class Multimedia{
+    constructor(url){
+        this._url = url;
+    }
+
+    get url(){
+        return this._url;
+    }
+
+    setInicio(){
+        return `Este método es para realizar un cambio en la URL del video.`;
+    }
+}
+
+class Reproductor extends Multimedia{
+    constructor(url, id){
+        super(url);
+        this._id = id;
+    }
+
+    playMultimedia(){}
+
+    setInicio(){}
+}
