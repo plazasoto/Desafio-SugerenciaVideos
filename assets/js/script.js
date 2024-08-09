5550d7dd0876ecb14cf4dd8dce42958a4359e91a
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-08-09 13:45:04 -0400

    Mostrando videos en documento HTML.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
-/*
-https://www.youtube.com/embed/6BUgYDmlVow
-*/
-
+/* IIFE controlador del atributo src */
 const moduloSrc = (()=>{
     function setSrcPrivate(url, id){
         document.getElementById(id).setAttribute("src",url);
@@ -13,6 +10,7 @@ const moduloSrc = (()=>{
     }
 })();
 
+/* Clases */
 class Multimedia{
     constructor(url){
         this._url = url;
@@ -38,6 +36,23 @@ class Reproductor extends Multimedia{
     }
 
     setInicio(tiempo){
-        this._url = `${this._url}?start=${tiempo}`;
+        this._url = `${this._url}?start=${tiempo}`;//&t=20
     }
 }
+
+/* IDs: musica peliculas series */
+/* URLs */
+let urlMusica = "https://www.youtube.com/embed/6BUgYDmlVow";
+let urlPeliculas = "https://www.youtube.com/embed/tw1Mfikk5M8";
+let urlSeries = "https://www.youtube.com/embed/APWXorE6h8U";
+
+/* Instancias de clases */
+let reproductorMusica = new Reproductor(urlMusica, "musica");
+let reproductorPeliculas = new Reproductor(urlPeliculas, "peliculas");
+let reproductorSeries = new Reproductor(urlSeries, "series");
+
+/* Mostrar videos */
+reproductorPeliculas.setInicio(20);
+reproductorMusica.playMultimedia();
+reproductorPeliculas.playMultimedia();
+reproductorSeries.playMultimedia();
